13bbe10bb04f6285417b9cdbff10877027d92b03
nemixu <s.r.seagrave@gmail.com>
2020-01-21 16:22:25 +0000

    adjusted the fetch for api and added catch for errors

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,15 +4,16 @@ fetch("https://opentdb.com/api_category.php")
         console.log(data);
         generateAnswers(data);
     })
+    .catch(err => console.log(err));
 
-function generateAnswers(whatever) {
-    whatever.trivia_categories.forEach(element => {
+function generateAnswers(questions) {
+    questions.trivia_categories.forEach(element => {
         fetchNewData(element.id);
     });
 }
-
+/*
 function fetchNewData(id) {
     fetch(`https://opentdb.com/api.php?amount=10&category=${id}`).then(res =>
         res.json().then(data => console.log(data))
     );
-}
+}*/
